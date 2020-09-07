04ece0d914e8d7c2d6b07af770a8d8db32571b16
darya-kondakova <44597105+darya-kondakova@users.noreply.github.com>
2020-09-07 20:58:21 +0700

    Add files via upload

---
+++ 2lab/lab.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+
+#define SUCCESS 0
+
+void pthread_error(const char *const msg, int error) {
+	fprintf(stderr, "%s: %s", msg, strerror(error));
+    exit(1);
+}
+
+void print_str(char *str) {
+	for (int i = 0; i < 10; i++) {
+		printf("%s\n", str);
+	}
+}
+
+void *child_body(void* arg) {
+	print_str("child");
+
+	return NULL;
+}
+
+int main() {
+	int error;
+	pthread_t child;
+	if ((error = pthread_create(&child, NULL, child_body, NULL)) != 0) {
+		pthread_error("pthread_create() failed", error);
+	}
+	
+	if ((error = pthread_join(child, NULL)) != 0) {
+		pthread_error("pthread_join() failed", error);
+	}
+
+	print_str("parent");
+
+	pthread_exit(NULL);
+}
