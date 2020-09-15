98a15907377d097eb6e3bebaf40a65a8cd0bfcea
darya-kondakova <44597105+darya-kondakova@users.noreply.github.com>
2020-09-15 15:29:24 +0700

    Update lab.c

--- 3lab/lab.c
+++ 3lab/lab.c
@@ -4,50 +4,43 @@
 #include <string.h>
 
 #define SUCCESS 0
+#define THREADS 4
+#define BUF_SIZE 256
 
 void pthread_error(const char *const msg, int error) {
-	fprintf(stderr, "%s: %s", msg, strerror(error));
+        fprintf(stderr, "%s: %s", msg, strerror(error));
     exit(1);
 }
 
 void *child_body(void* arg) {
-	for (char **line = arg; *line != NULL; line++) {
-		printf("%s\n", *line);
-	}
+        for (char **line = arg; *line != NULL; line++) {
+                printf("%s\n", *line);
+        }
 
-	return NULL;
+        return NULL;
 }
 
 int main() {
-	char *child_1[] = {"child 1 line 1", "child 1 line 2", "child 1 line 3", NULL},
-		*child_2[] = {"child 2 line 1", "child 2 line 2", "child 2 line 3", NULL},
-		*child_3[] = {"child 3 line 1", "child 3 line 2", "child 3 line 3", NULL},
-		*child_4[] = {"child 4 line 1", "child 4 line 2", "child 4 line 3", NULL};
+        char *child_p[THREADS][BUF_SIZE] = {{"child 1 line 1", "child 1 line 2", "child 1 line 3", NULL},
+                {"child 2 line 1", "child 2 line 2", "child 2 line 3", NULL},
+                {"child 3 line 1", "child 3 line 2", "child 3 line 3", NULL},
+                {"child 4 line 1", "child 4 line 2", "child 4 line 3", NULL}};
 
-	pthread_t child[4];
-	int error = pthread_create(&child[0], NULL, child_body, child_1);
-	if (error != 0) {
-		pthread_error("pthread_create(&child[0]) failed", error);
-	}
-	error = pthread_create(&child[1], NULL, child_body, child_2);
-	if (error != 0) {
-		pthread_error("pthread_create(&child[1]) failed", error);
-	}
-	error = pthread_create(&child[2], NULL, child_body, child_3);
-	if (error != 0) {
-		pthread_error("pthread_create(&child[2]) failed", error);
-	}
-	error = pthread_create(&child[3], NULL, child_body, child_4);
-	if (error != 0) {
-		pthread_error("pthread_create(&child[3]) failed", error);
-	}
-	
-	for (int i = 0; i < 4; i++) {
+        pthread_t child[THREADS];
+        int error;
+        for (int i = 0; i < THREADS; i++) {
+                error = pthread_create(&child[i], NULL, child_body, child_p[i]);
+                if (error != 0) {
+                        pthread_error("pthread_create(&child[0]) failed", error);
+                }
+        }
+
+        for (int i = 0; i < THREADS; i++) {
                 error = pthread_join(child[i], NULL);
                 if (error != SUCCESS) {
                         pthread_error("pthread_join() failed", error);
                 }
         }
-	
-	pthread_exit(NULL);
+
+        pthread_exit(NULL);
 }
