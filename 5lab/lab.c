0cab94e624ca72f3267e509f092aa8dd3b516c3e
darya-kondakova <44597105+darya-kondakova@users.noreply.github.com>
2020-09-22 11:45:07 +0700

    Update lab.c

--- 5lab/lab.c
+++ 5lab/lab.c
@@ -8,36 +8,36 @@
 
 void pthread_error(const char *const msg, int error) {
 	fprintf(stderr, "%s: %s", msg, strerror(error));
-    exit(1);
+	exit(1);
 }
 
 void child_handler(void* arg) {
-    printf("\nchild canceled\n");
+	printf("\nchild canceled\n");
 }
 
 void *child_body(void* arg) {
-    pthread_cleanup_push(child_handler, NULL);
+	pthread_cleanup_push(child_handler, NULL);
 
-    for (;;) {
-        write(0, "child\n", 6);
-    }
+	for (;;) {
+		write(0, "child\n", 6);
+	}
 
-    pthread_cleanup_pop(0);
+	pthread_cleanup_pop(0);
 
-    return NULL;
+	return NULL;
 }
 
 int main() {
 	pthread_t child;
 	int error = pthread_create(&child, NULL, child_body, NULL);
-	if (error != 0) {
+	if (error != SUCCESS) {
 		pthread_error("pthread_create() failed", error);
 	}
 	
 	sleep(2);
 
 	error = pthread_cancel(child);
-	if (error != 0) {
+	if (error != SUCCESS) {
 		pthread_error("pthread_cancel() failed", error);
 	}	
 
