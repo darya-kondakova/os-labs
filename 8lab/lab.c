4a4e5a9a0e5199c950d03921c96d9d7fd41fb374
darya-kondakova <44597105+darya-kondakova@users.noreply.github.com>
2020-10-13 14:08:52 +0700

    Update lab.c

--- 8lab/lab.c
+++ 8lab/lab.c
@@ -1,5 +1,7 @@
+#define _XOPEN_SOURCE 600
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <pthread.h>
 #include <signal.h>
 #include <errno.h>
@@ -7,52 +9,56 @@
 
 #define CHECK_STEP 1000000
 #define MAX_THREADS_AMOUNT 800
+#define SUCCESS 0
 #define ERRNO_SET 0
 #define ERROR_EXIT 1
 
 int threads_amount;
 int exit_flag = 0;
+pthread_barrier_t barrier;
 
 void error_exit(const char *const msg, int error) {
     if (error != ERRNO_SET) {
-		fprintf(stderr, "%s: %s", msg, strerror(error));
+        fprintf(stderr, "%s: %s", msg, strerror(error));
     } else {
-		if (errno == 0) {
-			fprintf(stderr, "%s\n", msg);
-		} else {
-			perror(msg);
-		}
+        if (errno == 0) {
+            fprintf(stderr, "%s\n", msg);
+        } else {
+            perror(msg);
+        }
     }
     exit(ERROR_EXIT);
 }
 
 void check_args(int argc, char **argv) {
-	if (argc < 2) {
-		error_exit("Usage: ./a.out threads_amount", ERRNO_SET);
-	}
-	threads_amount = atoi(argv[1]); 
-	if (threads_amount < 1 || MAX_THREADS_AMOUNT < threads_amount) {
-		error_exit("invalid number of threads", ERRNO_SET);
-	}
+    if (argc < 2) {
+        error_exit("Usage: ./a.out threads_amount", ERRNO_SET);
+    }
+    threads_amount = atoi(argv[1]);
+    if (threads_amount < 1 || MAX_THREADS_AMOUNT < threads_amount) {
+        error_exit("invalid number of threads", ERRNO_SET);
+    }
 }
 
 typedef struct thread_struct {
-	int thread_num;
-	double res;
+    int thread_num;
+    double res;
 } thread_t;
 
 void *pi_calc(void* arg) {
-	double pi_part = 0.0;
-	for (double j = 0; ; j++)  {
-		if (exit_flag) {
-			((thread_t *)arg)->res = pi_part;
-			pthread_exit(NULL);
-		}
-		for (double i = ((thread_t *)arg)->thread_num; i < CHECK_STEP; i += threads_amount) {
-			pi_part += 1 / (4.0 * (j * CHECK_STEP + i) + 1.0);
-			pi_part -= 1 / (4.0 * (j * CHECK_STEP + i) + 3.0);
-		}
-	}
+    double pi_part = 0.0;
+    for (double j = 0; ; j++)  {
+        pthread_barrier_wait(&barrier);
+        if (exit_flag) {
+            ((thread_t *)arg)->res = pi_part;
+            pthread_exit(NULL);
+        }
+        pthread_barrier_wait(&barrier);
+        for (double i = ((thread_t *)arg)->thread_num; i < CHECK_STEP; i += threads_amount) {
+            pi_part += 1 / (4.0 * (j * CHECK_STEP + i) + 1.0);
+            pi_part -= 1 / (4.0 * (j * CHECK_STEP + i) + 3.0);
+        }
+    }
 }
 
 void exit_sig() {
@@ -60,41 +66,50 @@ void exit_sig() {
 }
 
 int main(int argc, char **argv) {
-	int error;
-	check_args(argc, argv);
+    check_args(argc, argv);
 
-	sigset(SIGINT, exit_sig);
+    sigset(SIGINT, exit_sig);
 
-	pthread_t *child = (pthread_t *)malloc(threads_amount * sizeof(pthread_t));
-	if (child == NULL) {
-		error_exit("failed to allocate memory for threads", ERRNO_SET);
-	}
+    int error = pthread_barrier_init(&barrier, NULL, threads_amount);
+    if (error != SUCCESS) {
+        error_exit("pthread_barrier_init() failed", error);
+    }
 
-	thread_t *thread_struct = (thread_t *)malloc(threads_amount * sizeof(thread_t));
-	if (thread_struct == NULL) {
+    pthread_t *child = (pthread_t *)malloc(threads_amount * sizeof(pthread_t));
+    if (child == NULL) {
+        error_exit("failed to allocate memory for threads", ERRNO_SET);
+    }
+
+    thread_t *thread_struct = (thread_t *)malloc(threads_amount * sizeof(thread_t));
+    if (thread_struct == NULL) {
         error_exit("failed to allocate memory for thread_struct", ERRNO_SET);
     }
 
-	for (int i = 0; i < threads_amount; i++) {
-		thread_struct[i].thread_num = i;
-		error = pthread_create(&child[i], NULL, pi_calc, &thread_struct[i]);
-		if (error != 0) {
-			error_exit("pthread_create() failed", error);
-		}
-	}
-	
-	double pi = 0.0;
-	for (int i = 0; i < threads_amount; i++) {
-		if ((error = pthread_join(child[i], NULL)) != 0) {
-			error_exit("pthread_join() failed", error);
-		}
-		pi += thread_struct[i].res;
-	}
+    for (int i = 0; i < threads_amount; i++) {
+        thread_struct[i].thread_num = i;
+        error = pthread_create(&child[i], NULL, pi_calc, &thread_struct[i]);
+        if (error != SUCCESS) {
+            error_exit("pthread_create() failed", error);
+        }
+    }
 
-	printf("\nπ = %.15lf\n", pi * 4);
+    double pi = 0.0;
+    for (int i = 0; i < threads_amount; i++) {
+        error = pthread_join(child[i], NULL);
+        if (error != SUCCESS) {
+            error_exit("pthread_join() failed", error);
+        }
+        pi += thread_struct[i].res;
+    }
 
-	free(child);
-	free(thread_struct);
+    printf("\nπ = %.15lf\n", pi * 4);
 
-	pthread_exit(NULL);
+    error = pthread_barrier_destroy(&barrier);
+    if (error != SUCCESS) {
+        error_exit("pthread_barrier_destroy() failed", error);
+    }
+    free(child);
+    free(thread_struct);
+
+    pthread_exit(NULL);
 }
