3a3a87ad5c8793d714d743f5949d9b5cae88348d
darya-kondakova <44597105+darya-kondakova@users.noreply.github.com>
2020-10-18 16:51:08 +0700

    Update lab.c

--- 22lab/lab.c
+++ 22lab/lab.c
@@ -1,3 +1,4 @@
+#define _XOPEN_SOURCE 600
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -5,11 +6,18 @@
 #include <semaphore.h>
 #include <errno.h>
 #include <string.h>
+#include <signal.h>
 
 #define SUCCESS 0
 #define FAIL -1
 #define ERRNO_SET 0
 #define ERROR_EXIT 1
+
+#define MAX_A 100
+#define MAX_B 80
+#define MAX_C 60
+#define MAX_AB 60
+#define MAX_WIDGET 3
 
 void error_exit(const char *const msg, int error) {
     if (error != ERRNO_SET) {
@@ -24,103 +32,148 @@ void error_exit(const char *const msg, int error) {
     exit(ERROR_EXIT);
 }
 
-sem_t sem[4];   //sem[0] for A
+sem_t sem[5];   //sem[0] for A
                 //sem[1] for B
                 //sem[2] for C
                 //sem[3] for AB
 
+pthread_t   creater[4];
+
 void* create_A(void* arg) {
     for (;;) {
-        sleep(1);
-        int error = sem_post(&sem[0]);
-        if (error == FAIL) {
-            error_exit("sem_post() failed: unable to create A", ERRNO_SET);
+        pthread_testcancel();
+        int A;
+        sem_getvalue(&sem[0], &A);
+        if (A != MAX_A) {
+            sleep(1);
+            int error = sem_post(&sem[0]);
+            if (error == FAIL) {
+                error_exit("sem_post() failed: unable to create A", ERRNO_SET);
+            }
+            printf("new A\n");
         }
-        printf("new A\n");
     }
 }
 
 void* create_B(void* arg) {
     for (;;) {
-        sleep(2);
-        int error = sem_post(&sem[1]);
-        if (error == FAIL) {
-            error_exit("sem_post() failed: unable to create B", ERRNO_SET);
+        pthread_testcancel();
+        int B;
+        sem_getvalue(&sem[1], &B);
+        if (B != MAX_B) {
+            sleep(2);
+            int error = sem_post(&sem[1]);
+            if (error == FAIL) {
+                error_exit("sem_post() failed: unable to create B", ERRNO_SET);
+            }
+            printf("new B\n");
         }
-        printf("new B\n");
     }
 }
 
 void* create_C(void* arg) {
     for (;;) {
-        sleep(3);
-        int error = sem_post(&sem[2]);
-        if (error == FAIL) {
-            error_exit("sem_post() failed: unable to create C", ERRNO_SET);
+        pthread_testcancel();
+        int C;
+        sem_getvalue(&sem[2], &C);
+        if (C != MAX_C) {
+            sleep(3);
+            int error = sem_post(&sem[2]);
+            if (error == FAIL) {
+                error_exit("sem_post() failed: unable to create C", ERRNO_SET);
+            }
+            printf("new C\n");
         }
-        printf("new C\n");
     }
 }
 
 void* create_AB(void* arg) {
     for (;;) {
-        int error = sem_wait(&sem[0]);
-        if (error == FAIL) {
-            error_exit("sem_wait() failed: unable to use A", ERRNO_SET);
+        pthread_testcancel();
+        int AB;
+        sem_getvalue(&sem[3], &AB);
+        if (AB != MAX_AB) {
+            int error = sem_wait(&sem[0]);
+            if (error == FAIL) {
+                error_exit("sem_wait() failed: unable to use A", ERRNO_SET);
+            }
+            error = sem_wait(&sem[1]);
+            if (error == FAIL) {
+                error_exit("sem_wait() failed: unable to use B", ERRNO_SET);
+            }
+            error = sem_post(&sem[3]);
+            if (error == FAIL) {
+                error_exit("sem_post() failed: unable to create AB", ERRNO_SET);
+            }
+            printf("new AB\n");
         }
-        error = sem_wait(&sem[1]);
-        if (error == FAIL) {
-            error_exit("sem_wait() failed: unable to use B", ERRNO_SET);
-        }
-        error = sem_post(&sem[3]);
-        if (error == FAIL) {
-            error_exit("sem_post() failed: unable to create AB", ERRNO_SET);
-        }
-        printf("new AB\n");
     }
 }
 
 void* create_widget() {
     for (;;) {
-        int error = sem_wait(&sem[3]);
-        if (error == FAIL) {
-            error_exit("sem_wait() failed: unable to use AB", ERRNO_SET);
+        pthread_testcancel();
+        int widget;
+        sem_getvalue(&sem[4], &widget);
+        if (widget != MAX_WIDGET) {
+            int error = sem_wait(&sem[3]);
+            if (error == FAIL) {
+                error_exit("sem_wait() failed: unable to use AB", ERRNO_SET);
+            }
+            error = sem_wait(&sem[2]);
+            if (error == FAIL) {
+                error_exit("sem_wait() failed: unable to use C", ERRNO_SET);
+            }
+            error = sem_post(&sem[4]);
+            if (error == FAIL) {
+                error_exit("sem_post() failed: unable to create widget", ERRNO_SET);
+            }
+            printf("new widget\n");
         }
-        error = sem_wait(&sem[2]);
-        if (error == FAIL) {
-            error_exit("sem_wait() failed: unable to use C", ERRNO_SET);
-        }
-        printf("new widget\n");
     }
 }
 
-int main() {
-    pthread_t   creating_A,
-                creating_B,
-                creating_C,
-                creating_AB;
-
+void exit_sig() {
     int error;
     for (int i = 0; i < 4; i++) {
+        error = pthread_cancel(creater[i]);
+        if (error != SUCCESS) {
+            error_exit("pthread_cancel() failed", error);
+        }
+    }
+    for (int i = 0; i < 5; i++) {
+        error = sem_destroy(&sem[i]);
+        if (error == FAIL) {
+            error_exit("sem_destroy() failed", ERRNO_SET);
+        }
+    }
+    pthread_exit(NULL);
+}
+
+int main() {
+    sigset(SIGINT, exit_sig);
+
+    int error;
+    for (int i = 0; i < 5; i++) {
         error = sem_init(&sem[i], 0, 0);
         if (error == FAIL) {
             error_exit("sem_init() failed: unable to initialize semaphore", ERRNO_SET);
         }
     }
 
-    error = pthread_create(&creating_A, NULL, create_A, NULL);
+    error = pthread_create(&creater[0], NULL, create_A, NULL);
     if (error != SUCCESS) {
         error_exit("pthread_create() failed", error);
     }
-    error = pthread_create(&creating_B, NULL, create_B, NULL);
+    error = pthread_create(&creater[1], NULL, create_B, NULL);
     if (error != SUCCESS) {
         error_exit("pthread_create() failed", error);
     }
-    error = pthread_create(&creating_C, NULL, create_C, NULL);
+    error = pthread_create(&creater[2], NULL, create_C, NULL);
     if (error != SUCCESS) {
         error_exit("pthread_create() failed", error);
     }
-    error = pthread_create(&creating_AB, NULL, create_AB, NULL);
+    error = pthread_create(&creater[3], NULL, create_AB, NULL);
     if (error != SUCCESS) {
         error_exit("pthread_create() failed", error);
     }
