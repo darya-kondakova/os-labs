f27238857d911f5b08daaa6926cd58e0c3d562b8
darya-kondakova <44597105+darya-kondakova@users.noreply.github.com>
2020-10-27 14:27:01 +0700

    Update lab.c

--- 9lab/lab.c
+++ 9lab/lab.c
@@ -29,50 +29,50 @@ pthread_mutex_t forks[PHILO];
 pthread_mutex_t foodlock;
 
 int food_on_table() {
-    static int food = FOOD;
+	static int food = FOOD;
 
-    pthread_mutex_lock(&foodlock);
-    if (food > 0) {
-        food--;
-    }
-    int myfood = food;
-    pthread_mutex_unlock(&foodlock);
+	pthread_mutex_lock(&foodlock);
+    	if (food > 0) {
+        	food--;
+    	}
+    	int myfood = food;
+    	pthread_mutex_unlock(&foodlock);
 
-    return myfood;
+    	return myfood;
 }
 
 void get_fork(int phil, int fork, char *hand) {
-    pthread_mutex_lock(&forks[fork]);
-    printf("Philosopher %d: got %s fork %d\n", phil, hand, fork);
+	pthread_mutex_lock(&forks[fork]);
+	printf("Philosopher %d: got %s fork %d\n", phil, hand, fork);
 }
 
 void down_forks(int f1, int f2) {
-    pthread_mutex_unlock(&forks[f1]);
-    pthread_mutex_unlock(&forks[f2]);
+	pthread_mutex_unlock(&forks[f1]);
+	pthread_mutex_unlock(&forks[f2]);
 }
 
 void *philosopher(void *num) {
-    int id = (int)num;
-    printf("Philosopher %d sitting down to dinner.\n", id);
-    int left_fork = id,
-    	right_fork = (id + 1) % 5; /* Wrap around the forks. */
+	int id = (int)num;
+	printf("Philosopher %d sitting down to dinner.\n", id);
+	int left_fork = id,
+	right_fork = (id + 1) % 5; /* Wrap around the forks. */
 
 	int f;
-    while ((f = food_on_table())) {
+	while ((f = food_on_table())) {
 		printf("Philosopher %d: reflects.\n", id);
 		usleep(DELAY * (FOOD - f + 1));
 
-        printf("Philosopher %d: get dish %d.\n", id, f);
-        get_fork(id, left_fork, "left ");
-        get_fork(id, right_fork, "right");
+        	printf("Philosopher %d: get dish %d.\n", id, f);
+        	get_fork(id, left_fork, "left ");
+        	get_fork(id, right_fork, "right");
 
-        printf("Philosopher %d: eating.\n", id);
-        usleep(DELAY * (FOOD - f + 1));
-        down_forks(left_fork, right_fork);
-    }
-    printf("Philosopher %d is done eating.\n", id);
+        	printf("Philosopher %d: eating.\n", id);
+        	usleep(DELAY * (FOOD - f + 1));
+        	down_forks(left_fork, right_fork);
+    	}
+   	printf("Philosopher %d is done eating.\n", id);
 
-    return NULL;
+    	return NULL;
 }
 
 int main(int argn, char **argv) {
@@ -80,9 +80,9 @@ int main(int argn, char **argv) {
     if (error != SUCCESS) {
 		error_exit("pthread_mutex_init() failed", error);
 	}
-    for (int i = 0; i < PHILO; i++) {
+   	for (int i = 0; i < PHILO; i++) {
 		error = pthread_mutex_init(&forks[i], NULL); 
-        if (error != SUCCESS) {
+        	if (error != SUCCESS) {
 			error_exit("pthread_mutex_init() failed", error);
 		}
 	}
@@ -94,17 +94,17 @@ int main(int argn, char **argv) {
 		}
 	}
     for (int i = 0; i < PHILO; i++) {
-        error = pthread_join(phils[i], NULL);
+		error = pthread_join(phils[i], NULL);
 		if (error != SUCCESS) {
 			error_exit("pthread_join() failed", error);
 		}
 	}
 	for (int i = 0; i < PHILO; i++) {
-        error = pthread_mutex_destroy(&forks[i]);
-        if (error != SUCCESS) {
-            error_exit("pthread_mutex_destroy() failed", error);
-        }
-    }
+		error = pthread_mutex_destroy(&forks[i]);
+		if (error != SUCCESS) {
+			error_exit("pthread_mutex_destroy() failed", error);
+		}
+	}
 
     return NULL;
 }
