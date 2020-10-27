50c629938659f21d1bd1df44c237c78ec45abb54
darya-kondakova <44597105+darya-kondakova@users.noreply.github.com>
2020-10-27 14:27:46 +0700

    Update lab.c

--- 9lab/lab.c
+++ 9lab/lab.c
@@ -77,7 +77,7 @@ void *philosopher(void *num) {
 
 int main(int argn, char **argv) {
 	int error = pthread_mutex_init(&foodlock, NULL);
-    if (error != SUCCESS) {
+   	if (error != SUCCESS) {
 		error_exit("pthread_mutex_init() failed", error);
 	}
    	for (int i = 0; i < PHILO; i++) {
@@ -87,13 +87,13 @@ int main(int argn, char **argv) {
 		}
 	}
 	pthread_t phils[PHILO];
-    for (int i = 0; i < PHILO; i++) {
+   	 for (int i = 0; i < PHILO; i++) {
 		error = pthread_create(&phils[i], NULL, philosopher, (void *)((long)i));
     	if (error != SUCCESS) {
 			error_exit("pthread_create() failed", error);
 		}
 	}
-    for (int i = 0; i < PHILO; i++) {
+    	for (int i = 0; i < PHILO; i++) {
 		error = pthread_join(phils[i], NULL);
 		if (error != SUCCESS) {
 			error_exit("pthread_join() failed", error);
@@ -106,5 +106,5 @@ int main(int argn, char **argv) {
 		}
 	}
 
-    return NULL;
+    	return NULL;
 }
