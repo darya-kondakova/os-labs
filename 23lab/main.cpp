df667440c9d37288d4526cef07ff53ccaac52756
darya-kondakova <44597105+darya-kondakova@users.noreply.github.com>
2020-12-08 15:36:38 +0700

    Update main.cpp

--- 23lab/main.cpp
+++ 23lab/main.cpp
@@ -13,24 +13,24 @@
 #define SUCCESS 0
 
 void *producer(void *arg) {
-    Queue *queue = (Queue *)arg;
+	Queue *queue = (Queue *)arg;
 	for(int i = 0; ; i++) {
-        usleep(1000);
+		usleep(1000);
 		char buf[40];
 		sprintf(buf, "Message %d from thread %d", i, pthread_self());
 		queue->mymsgput(buf);
-        printf("put\n");
+		printf("put\n");
 	}
 }
 
 void *consumer(void *arg) {
-    Queue *queue = (Queue *)arg;
+	Queue *queue = (Queue *)arg;
 	for (;;) {
-        sleep(2);
+        	sleep(2);
 		char buf[41];
-        queue->mymsgget(buf, sizeof(buf));
+        	queue->mymsgget(buf, sizeof(buf));
 		printf("Received by thread %d: %s\n", pthread_self(), buf);
-        printf("get\n");
+        	printf("get\n");
 	}
 }
 
@@ -43,42 +43,42 @@ void exit_sig(int sig) {
 int main(int argc, char **argv) {
 	sigset(SIGINT, exit_sig);
 
-    Queue *queue = new Queue();
+	Queue *queue = new Queue();
 
-    pthread_t threads[PRODUCERS + CONSUMERS];
-    int error;
-    for (int i = 0; i < PRODUCERS; i++) {
-        error = pthread_create(&threads[i], NULL, producer, queue);
-        if (error != SUCCESS) {
-            error_exit("pthread_create() failed", error);
-        }
-    }
+	pthread_t threads[PRODUCERS + CONSUMERS];
+	int error;
+	for (int i = 0; i < PRODUCERS; i++) {
+		error = pthread_create(&threads[i], NULL, producer, queue);
+		if (error != SUCCESS) {
+			error_exit("pthread_create() failed", error);
+		}
+	}
 	for (int i = 0; i < CONSUMERS; i++) {
-        error = pthread_create(&threads[PRODUCERS + i], NULL, consumer, queue);
-        if (error != SUCCESS) {
-            error_exit("pthread_create() failed", error);
-        }
+        	error = pthread_create(&threads[PRODUCERS + i], NULL, consumer, queue);
+        	if (error != SUCCESS) {
+            		error_exit("pthread_create() failed", error);
+        	}
 	}
 
-    while (!interrupt) {}
+	while (!interrupt) {}
 
 	queue->mymsgdrop();
 
-    for (int i = 0; i < PRODUCERS + CONSUMERS; i++) {
-        error = pthread_cancel(threads[i]);
-        if (error != SUCCESS) {
-            error_exit("pthread_cancel() failed", error);
-        }
-    }
-
 	for (int i = 0; i < PRODUCERS + CONSUMERS; i++) {
-        error = pthread_join(threads[i], NULL);
-        if (error != SUCCESS) {
-            error_exit("pthread_join() failed", error);
-        }
+		error = pthread_cancel(threads[i]);
+		if (error != SUCCESS) {
+			error_exit("pthread_cancel() failed", error);
+		}
 	}
 
-    delete queue;
+	for (int i = 0; i < PRODUCERS + CONSUMERS; i++) {
+		error = pthread_join(threads[i], NULL);
+		if (error != SUCCESS) {
+			error_exit("pthread_join() failed", error);
+		}
+	}
+
+	delete queue;
 
 	return 0;	
 }
