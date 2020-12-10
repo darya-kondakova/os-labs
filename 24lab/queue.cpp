41b347b889eeffbcab25e377e486dc15ada6a841
darya-kondakova <44597105+darya-kondakova@users.noreply.github.com>
2020-12-10 20:21:18 +0700

    Add files via upload

--- 24lab/queue.cpp
+++ 24lab/queue.cpp
@@ -3,25 +3,74 @@
 #include <string.h>
 #include <pthread.h>
 #include "queue.h"
+#include "error_exit.h"
+
+void init(pthread_cond_t *cond, pthread_mutex_t *mutex) {
+    int error = pthread_cond_init(cond, NULL);
+    if (error != SUCCESS) {
+        error_exit("pthread_cond_init() failed", error);
+    }
+    error = pthread_mutex_init(mutex, NULL);
+    if (error != SUCCESS) {
+        error_exit("pthread_mutex_init() failed", error);
+    }
+}
+
+void my_pthread_mutex_lock(pthread_mutex_t *mutex) {
+    int error = pthread_mutex_lock(mutex);
+    if (error != SUCCESS) {
+        error_exit("pthread_mutex_lock() failed", error);
+    }
+}
+
+void my_pthread_mutex_unlock(pthread_mutex_t *mutex) {
+    int error = pthread_mutex_unlock(mutex);
+    if (error != SUCCESS) {
+        error_exit("pthread_mutex_unlock() failed", error);
+    }
+}
+
+void my_pthread_cond_wait(pthread_cond_t *cond, pthread_mutex_t *mutex) {
+    int error = pthread_cond_wait(cond, mutex);
+    if (error != SUCCESS) {
+        error_exit("pthread_cond_wait() failed", error);
+    }
+}
+
+void my_pthread_cond_broadcast(pthread_cond_t *cond) {
+    int error = pthread_cond_broadcast(cond);
+    if (error != SUCCESS) {
+        error_exit("pthread_cond_broadcast() failed", error);
+    }
+}
+
+void destroy(pthread_cond_t *cond, pthread_mutex_t *mutex) {
+    int error = pthread_cond_destroy(cond);
+    if (error != SUCCESS) {
+        error_exit("pthread_cond_destroy() failed", error);
+    }
+    error = pthread_mutex_destroy(mutex);
+    if (error != SUCCESS) {
+        error_exit("pthread_mutex_init() failed", error);
+    }
+}
 
 Queue::Queue() {
     head = NULL;
     tail = NULL;
     droped = false;
     size = 0;
-    pthread_cond_init(&cond, NULL);
-    pthread_mutex_init(&queue_mutex, NULL);
+    init(&cond, &queue_mutex);
 }
 
 Queue::~Queue() {
-    pthread_cond_destroy(&cond);
-    pthread_mutex_destroy(&queue_mutex);
+    destroy(&cond, &queue_mutex);
 }
 
 void Queue::mymsgdrop() {
     droped = true;
-    pthread_cond_broadcast(&cond);
-    pthread_mutex_lock(&queue_mutex);
+    my_pthread_cond_broadcast(&cond);
+    my_pthread_mutex_lock(&queue_mutex);
     struct queue_element *t = head;
     while (t) {
         struct queue_element *t1 = t->next;
@@ -29,16 +78,16 @@ void Queue::mymsgdrop() {
         t = t1;
     }
     size = 0;
-    pthread_mutex_unlock(&queue_mutex);
+    my_pthread_mutex_unlock(&queue_mutex);
 }
 
 int Queue::mymsgput(char *msg) {
-    pthread_mutex_lock(&queue_mutex);
+    my_pthread_mutex_lock(&queue_mutex);
     while (size >= 10 && !droped) {
-        pthread_cond_wait(&cond, &queue_mutex);
+        my_pthread_cond_wait(&cond, &queue_mutex);
     }
     if (droped) {
-        pthread_mutex_unlock(&queue_mutex);
+        my_pthread_mutex_unlock(&queue_mutex);
         return 0;
     }
     q_elem *t = (q_elem*)malloc(sizeof(q_elem));
@@ -53,18 +102,18 @@ int Queue::mymsgput(char *msg) {
         tail = t;
     }
     size++;
-    pthread_cond_broadcast(&cond);
-    pthread_mutex_unlock(&queue_mutex);
+    my_pthread_cond_broadcast(&cond);
+    my_pthread_mutex_unlock(&queue_mutex);
     return strlen(t->buf);
 }
 
 int Queue::mymsgget(char *buf, size_t bufsize) {
-    pthread_mutex_lock(&queue_mutex);
+    my_pthread_mutex_lock(&queue_mutex);
     while (size <= 0 && !droped) {
-        pthread_cond_wait(&cond, &queue_mutex);
+        my_pthread_cond_wait(&cond, &queue_mutex);
     }
     if (droped) {
-        pthread_mutex_unlock(&queue_mutex);
+        my_pthread_mutex_unlock(&queue_mutex);
         return 0;
     }
     struct queue_element *t = head;
@@ -74,11 +123,11 @@ int Queue::mymsgget(char *buf, size_t bufsize) {
         head = t->next;
         head->prev = NULL;
     }
-    pthread_mutex_unlock(&queue_mutex);
+    my_pthread_mutex_unlock(&queue_mutex);
     strncpy(buf, t->buf, bufsize-1);
     buf[strlen(buf)] = '\0';
     free(t);
     size--;
-    pthread_cond_broadcast(&cond);
+    my_pthread_cond_broadcast(&cond);
     return strlen(buf);
 }
