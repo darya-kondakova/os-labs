4bb830ec612a05e3ca8b85fb8532818ef741e7e7
darya-kondakova <44597105+darya-kondakova@users.noreply.github.com>
2020-12-10 20:21:58 +0700

    Add files via upload

--- 23lab/queue.cpp
+++ 23lab/queue.cpp
@@ -3,46 +3,81 @@
 #include <string.h>
 #include <semaphore.h>
 #include "queue.h"
+#include "error_exit.h"
+
+void my_sem_init(sem_t* put_sem, sem_t* get_sem, sem_t* queue_sem) {
+    int error = sem_init(put_sem, 0, 10);
+    if (error != SUCCESS) {
+        error_exit("sem_init() failed", ERRNO_SET);
+    }
+    error = sem_init(get_sem, 0, 0);
+    if (error != SUCCESS) {
+        error_exit("sem_init() failed", ERRNO_SET);
+    }
+    error = sem_init(queue_sem, 0, 1);
+    if (error != SUCCESS) {
+        error_exit("sem_init() failed", ERRNO_SET);
+    }
+}
+
+void my_sem_wait(sem_t* sem) {
+    int error = sem_wait(sem);
+    if (error != SUCCESS) {
+        error_exit("sem_wait() failed", ERRNO_SET);
+    }
+}
+
+void my_sem_post(sem_t* sem) {
+    int error = sem_post(sem);
+    if (error != SUCCESS) {
+        error_exit("sem_post() failed", ERRNO_SET);
+    }
+}
+
+void my_sem_destroy(sem_t* sem) {
+    int error = sem_destroy(sem);
+    if (error != SUCCESS) {
+        error_exit("sem_destroy() failed", ERRNO_SET);
+    }
+}
 
 Queue::Queue() {
     head = NULL;
     tail = NULL;
     droped = false;
-    sem_init(&put_sem, 0, 10);
-    sem_init(&get_sem, 0, 0);
-    sem_init(&queue_sem, 0, 1);
+    my_sem_init(&put_sem, &get_sem, &queue_sem);
 }
 
 Queue::~Queue() {
-    sem_destroy(&put_sem);
-    sem_destroy(&get_sem);
-    sem_destroy(&queue_sem);
+    my_sem_destroy(&put_sem);
+    my_sem_destroy(&get_sem);
+    my_sem_destroy(&queue_sem);
 }
 
 void Queue::mymsgdrop() {
+    my_sem_wait(&queue_sem);
     droped = true;
-    sem_wait(&queue_sem);
-    sem_post(&put_sem);
-    sem_post(&get_sem);
+    my_sem_post(&put_sem);
+    my_sem_post(&get_sem);
     struct queue_element *t = head;
     while (t) {
         struct queue_element *t1 = t->next;
         free(t);
         t = t1;
     }
-    sem_post(&queue_sem);
+    my_sem_post(&queue_sem);
 }
 
 int Queue::mymsgput(char *msg) {
-    sem_wait(&put_sem);
-    sem_wait(&queue_sem);
+    my_sem_wait(&put_sem);
+    my_sem_wait(&queue_sem);
     if (droped) {
-        sem_post(&put_sem);
-        sem_post(&queue_sem);
+        my_sem_post(&put_sem);
+        my_sem_post(&queue_sem);
         return 0;
     }
-    q_elem *t = (q_elem*)malloc(sizeof(q_elem));
-    strncpy(t->buf, msg, sizeof(t->buf)-1);
+    q_elem *t = (q_elem *) malloc(sizeof(q_elem));
+    strncpy(t->buf, msg, sizeof(t->buf) - 1);
     t->buf[strlen(t->buf)] = '\0';
     t->prev = tail;
     t->next = NULL;
@@ -52,17 +87,17 @@ int Queue::mymsgput(char *msg) {
         tail->next = t;
         tail = t;
     }
-    sem_post(&get_sem);
-    sem_post(&queue_sem);
+    my_sem_post(&get_sem);
+    my_sem_post(&queue_sem);
     return strlen(t->buf);
 }
 
 int Queue::mymsgget(char *buf, size_t bufsize) {
-    sem_wait(&get_sem);
-    sem_wait(&queue_sem);
+    my_sem_wait(&get_sem);
+    my_sem_wait(&queue_sem);
     if (droped) {
-        sem_post(&get_sem);
-        sem_post(&queue_sem);
+        my_sem_post(&get_sem);
+        my_sem_post(&queue_sem);
         return 0;
     }
     struct queue_element *t = head;
@@ -72,10 +107,11 @@ int Queue::mymsgget(char *buf, size_t bufsize) {
         head = t->next;
         head->prev = NULL;
     }
-    sem_post(&queue_sem);
-    strncpy(buf, t->buf, bufsize-1);
+    my_sem_post(&queue_sem);
+    strncpy(buf, t->buf, bufsize - 1);
     buf[strlen(buf)] = '\0';
     free(t);
-    sem_post(&put_sem);
+    my_sem_post(&put_sem);
     return strlen(buf);
 }
+
