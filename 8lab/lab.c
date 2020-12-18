f1001aeacf9ce5e69d265ec3a976b2555b2b9e41
darya-kondakova <44597105+darya-kondakova@users.noreply.github.com>
2020-12-18 15:19:10 +0700

    Update lab.c

--- 8lab/lab.c
+++ 8lab/lab.c
@@ -15,7 +15,16 @@
 
 int threads_amount;
 int exit_flag = 0;
+int threads_exit_flag = 0;
 pthread_barrier_t barrier;
+
+/*
+    The maximum number of significant iterations:
+        size_t overflow or double underflow
+        are above this limit
+*/
+const size_t iter_cnt_limit = 
+                sizeof(size_t) * 8 >= 53 ? ((size_t)1 << 53) : (size_t)(-1);
 
 void error_exit(const char *const msg, int error) {
     if (error != ERRNO_SET) {
@@ -30,29 +39,30 @@ void error_exit(const char *const msg, int error) {
     exit(ERROR_EXIT);
 }
 
-void check_args(int argc, char **argv) {
-    if (argc < 2) {
-        error_exit("Usage: ./a.out threads_amount", ERRNO_SET);
-    }
-    threads_amount = atoi(argv[1]);
-    if (threads_amount < 1 || MAX_THREADS_AMOUNT < threads_amount) {
-        error_exit("invalid number of threads", ERRNO_SET);
-    }
-}
-
 typedef struct thread_struct {
     int thread_num;
     double res;
 } thread_t;
 
+void my_pthread_barrier_wait() {
+    int error = pthread_barrier_wait(&barrier);
+    if (error != SUCCESS && error != PTHREAD_BARRIER_SERIAL_THREAD) {
+        error_exit("pthread_barrier_wait() failed", error);
+    }
+}
+
 void *pi_calc(void* arg) {
     double pi_part = 0.0;
     for (double j = 0; ; j++)  {
-        pthread_barrier_wait(&barrier);
         if (exit_flag) {
+            threads_exit_flag = 1;
+        }
+        my_pthread_barrier_wait();
+        if (threads_exit_flag) {
             ((thread_t *)arg)->res = pi_part;
             pthread_exit(NULL);
         }
+        my_pthread_barrier_wait();
         for (double i = ((thread_t *)arg)->thread_num; i < CHECK_STEP; i += threads_amount) {
             pi_part += 1 / (4.0 * (j * CHECK_STEP + i) + 1.0);
             pi_part -= 1 / (4.0 * (j * CHECK_STEP + i) + 3.0);
@@ -62,6 +72,16 @@ void *pi_calc(void* arg) {
 
 void exit_sig() {
     exit_flag = 1;
+}
+
+void check_args(int argc, char **argv) {
+    if (argc < 2) {
+        error_exit("Usage: ./a.out threads_amount", ERRNO_SET);
+    }
+    threads_amount = atoi(argv[1]);
+    if (threads_amount < 1 || MAX_THREADS_AMOUNT < threads_amount) {
+        error_exit("invalid number of threads", ERRNO_SET);
+    }
 }
 
 int main(int argc, char **argv) {
